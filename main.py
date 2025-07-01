f220de000626f6c9645101476d057f3ac2983c70
Galdino <pedro.wollmann@gmail.com>
2025-07-01 15:35:19 -0300

    teste das funcoes da UserController

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from bottle import route, run, template
+from controllers.user_controller import *
+from models.user import User
 
+
+aluno = User("0", "djota", "dota@email", "123")
+UserController.create_json_file("teste")
+#fazer ela ir para o filepath certo(diretorio data)
+aluno_dict = UserController.to_dict(aluno)
+UserController.save_user(aluno_dict)
+
+
+# @route("/") 
+# def home():
+#     return template("home")
+
+# run(debug=True,reloader=True,)
