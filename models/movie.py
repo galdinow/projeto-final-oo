dda99733bbaaf9c9d14fb7d5921fc703bd7ad0bb
Galdino <pedro.wollmann@gmail.com>
2025-07-01 15:40:20 -0300

    model movie criada

---
+++ models/movie.py
@@ -0,0 +1,8 @@
+class Movie:
+    def __init__(self, title, description, id,director,img_url):
+        self.title = title
+        self.descrition = description
+        self.id = id
+        self.director = director
+        self.img_url = img_url
+    
