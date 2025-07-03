2d07ca2ae1314edf3d26ca6016daf1bb88ab9590
Carol Becker <carolsmbecker@gmail.com>
2025-07-03 19:46:51 -0300

    rota de cadastro de filmes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from bottle import route, run, template
+from bottle import route, run, template, request, redirect
 from controllers.user_controller import *
 from models.user import User
+from models.movie import Movie
+from controllers.movie_controller import MovieController
 
 
 aluno = User("0", "djota", "dota@email", "123")
@@ -8,6 +10,16 @@ UserController.create_json_file("teste")
 #fazer ela ir para o filepath certo(diretorio data)
 aluno_dict = UserController.to_dict(aluno)
 UserController.save_user(aluno_dict)
+
+
+
+
+@route('/cadastrar-filme,', method='POST')
+def cadastrar_filme():
+    title = request.forms.get('title')
+    description = request.forms.get('description')
+    director = request.forms.get('director')
+    img_url = request.forms.get('img_url')
 
 
 # @route("/") 
--- models/movie.py
+++ models/movie.py
@@ -2,7 +2,7 @@ class Movie:
     def __init__(self, id, title, description,director,img_url):
         self.id = id
         self.title = title
-        self.descrition = description
+        self.description = description
         self.director = director
         self.img_url = img_url
     
