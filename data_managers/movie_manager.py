9e473654f2c830faa342c07256be03c9ddabbe99
Galdino <pedro.wollmann@gmail.com>
2025-07-04 00:31:41 -0300

    funcionalidades de achar o filme pelo id e carregar o arquivo json completas

--- data_managers/movie_manager.py
+++ data_managers/movie_manager.py
@@ -11,3 +11,19 @@ def save_movie(movie):
         updated_json.append(movie)
     with open("data/movies.json", "w") as file:
         json.dump(updated_json, file, indent=4)
+
+# sera se a funcao ta "magica" demais? com o filepath sendo selecionado sozinho?
+def load_movies():
+    with open("data/movies.json", "r") as file:
+        movie_list = json.load(file)
+        return movie_list
+
+
+#fazer uma funcao que le os filmes salvos em json e implementar ela nessa funcao
+# nao testei ainda
+# acho q a funcao nao retornar nada nao faz muito sentido
+def get_movie_by_id(id):
+    for filme in load_movies():
+        if filme["id"] == id:
+            return print(filme["title"])
+    return print("deu errado")
