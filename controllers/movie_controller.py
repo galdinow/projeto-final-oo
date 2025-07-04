ed16545b85c6156b09bb7c60acd1de9ba74fe67b
Carol Becker <carolsmbecker@gmail.com>
2025-07-03 21:40:52 -0300

    lista temporária de filmes

--- controllers/movie_controller.py
+++ controllers/movie_controller.py
@@ -2,6 +2,8 @@ from models.movie import Movie
 
 class MovieController:
 
+    filmes = [] #depois trocar pra json
+
     @staticmethod
     def to_dict(movie):
         return {"id":movie.id, "title":movie.title, "description": movie.description, "director":movie.director, "img_url":movie.img_url}
