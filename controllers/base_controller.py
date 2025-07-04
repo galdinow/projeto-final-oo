992b42993bd39b3db427c335233b057100b5216b
Galdino <pedro.wollmann@gmail.com>
2025-07-03 23:24:03 -0300

    metodos incorretos removidos

--- controllers/base_controller.py
+++ controllers/base_controller.py
@@ -1,17 +1,4 @@
-import json
-from pathlib import Path
-class BaseController:
+# import json
+# from pathlib import Path
+# class BaseController:
     
-    #mudar o path, e arrumar a identacao
-    @staticmethod
-    def create_json_file(filename):
-        json_list = []
-        with open(filename,mode="w") as file:
-            json.dump(json_list, file)
-
-    
-
-    @staticmethod
-    def create_path():
-        script_dir = Path(__file__).parent
-        print(script_dir)
