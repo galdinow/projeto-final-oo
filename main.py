4fd70589ccc4b5bba0240bc35dc3ac1751d079b1
Galdino <pedro.wollmann@gmail.com>
2025-07-03 23:27:50 -0300

    teste corrigido

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from controllers.user_controller import *
 from models.user import User
 from models.movie import Movie
 from controllers.movie_controller import MovieController
+from data_managers.user_manager import save_user, create_json_file
 
 
 aluno = User("0", "djota", "dota@email", "123")
-UserController.create_json_file("teste")
+create_json_file("teste")
 #fazer ela ir para o filepath certo(diretorio data)
-aluno_dict = UserController.to_dict(aluno)
-UserController.save_user(aluno_dict)
+aluno_dict = aluno.to_dict()
+save_user(aluno_dict)
 
 
 
