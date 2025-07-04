ae8b18dcdd2385f667212ae32ac210cc2f7b6bf4
Galdino <pedro.wollmann@gmail.com>
2025-07-03 23:26:23 -0300

    removidos metodos incorretos

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,22 +1,6 @@
-import json
-from models.user import User
-from controllers.base_controller import BaseController
+# import json
+# from models.user import User
+# from controllers.base_controller import BaseController
 
-class UserController(BaseController):
+# class UserController(BaseController):
     
-    # talvez fazer um controle de erros depois e arrumar identacao
-    @staticmethod
-    def save_user(user):
-        with open("teste", "r") as file:
-            existing_json = json.load(file)
-            updated_json = existing_json
-            updated_json.append(user)
-        with open("teste", "w") as file:
-            json.dump(updated_json, file)
-
-    
-
-    # mudar esse metodo depois para 
-    @staticmethod
-    def to_dict(user):
-        return {"id":user.user_id, "name": user.username, "email": user.email, "password": user.password}
