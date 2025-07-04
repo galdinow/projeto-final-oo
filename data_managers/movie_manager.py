45a5d856295f419fa048dc333385866735a1a5bc
Galdino <pedro.wollmann@gmail.com>
2025-07-03 23:50:34 -0300

    funcao de salvar filme adicionada

--- data_managers/movie_manager.py
+++ data_managers/movie_manager.py
@@ -1,1 +1,13 @@
-#criar as mesmas funcoes do user manager aq
+import json
+
+# talvez fazer um controle de erros depois 
+# ver se isso realmente tem que existir
+
+
+def save_movie(movie):
+    with open("data/movies.json", "r") as file:
+        existing_json = json.load(file)
+        updated_json = existing_json
+        updated_json.append(movie)
+    with open("data/movies.json", "w") as file:
+        json.dump(updated_json, file, indent=4)
