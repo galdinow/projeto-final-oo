15622ff47ca0af7ae32e07a6d3733c07e2d2e5f3
Galdino <pedro.wollmann@gmail.com>
2025-07-03 23:25:05 -0300

    heranca da BaseModel e metodo to_dict

--- models/movie.py
+++ models/movie.py
@@ -1,4 +1,6 @@
-class Movie:
+from models.base_model import BaseModel
+
+class Movie(BaseModel):
     def __init__(self, id, title, description,director,img_url):
         self.id = id
         self.title = title
@@ -6,3 +8,7 @@ class Movie:
         self.director = director
         self.img_url = img_url
     
+
+    
+    def to_dict(movie):
+        return {"id":movie.id, "title":movie.title, "description": movie.description, "director":movie.director, "img_url":movie.img_url}
--- models/user.py
+++ models/user.py
@@ -1,7 +1,11 @@
-class User:
+from models.base_model import BaseModel
+
+class User(BaseModel):
     def __init__(self, user_id, username, email, password):
         self.user_id = user_id
         self.username = username
         self.email = email
         self.password = password
-        
+    
+    def to_dict(user):
+        return {"id":user.user_id, "name": user.username, "email": user.email, "password": user.password}
