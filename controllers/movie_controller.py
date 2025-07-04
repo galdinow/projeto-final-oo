a4e65d756ae4174493dc50e68ad703f948932346
Carol Becker <carolsmbecker@gmail.com>
2025-07-03 21:26:28 -0300

    procura filmes por id

--- controllers/movie_controller.py
+++ controllers/movie_controller.py
@@ -9,3 +9,10 @@ class MovieController:
     @staticmethod
     def adicionar_filme(movie):
         MovieController.filmes.append(movie)
+
+    @staticmethod
+    def get_movie_by_id(id):
+        for filme in MovieController.filmes:
+            if filme.id == id:
+                return filme
+        return None
