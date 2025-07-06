fe0a8e06b5624882893e29884694876d986504ee
Galdino <pedro.wollmann@gmail.com>
2025-07-06 13:50:19 -0300

    criada a rota para login

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,23 @@
-# import json
-# from models.user import User
-# from controllers.base_controller import BaseController
+import json
+import random
+from models.user import User
+from controllers.base_controller import BaseController
+# ver se eu uso tudo isso msm dps
+from bottle import route, run, template, request, redirect
+from data_managers import user_manager
 
-# class UserController(BaseController):
+class UserController:
+    def __init__(self):
+        pass
     
+# ver dps se as rotas na verdade ficam aq
+@route("/cadastro-usuario", method="POST")
+def cadastro_usuario():
+    username = request.forms.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    # acho q tem um jeito melhor de fazer isso
+    id = random.randint(1000, 9999)
+
+    user = User(id, username, email, password)
+    user_manager.save_user(user)
