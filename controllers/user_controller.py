500ee9b5c30f28b82eb7c9661b731791edbb0e82
Galdino <pedro.wollmann@gmail.com>
2025-07-07 11:53:57 -0300

    comentarios retirados

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -2,7 +2,6 @@ import json
 import random
 from models.user import User
 from controllers.base_controller import BaseController
-# ver se eu uso tudo isso msm dps
 from bottle import route, run, template, request, redirect, Bottle
 from data_managers import user_manager
 
@@ -10,30 +9,22 @@ class UserController:
     def __init__(self):
         pass
     
-# ver dps se as rotas na verdade ficam aq
-    # def setup_routes(self, app):
-    #     app.route("/login", method="GET")
-
-
     def process_user(self):
         username = request.forms.get("username")
         email = request.forms.get("email")
         password = request.forms.get("password")
-        #tem um jeito melhor de fazer isso
         id = random.randint(1000, 9999)
 
         user = User(id, username, email, password)
         dict_user = user.to_dict()
         user_manager.save_user(dict_user)
         return redirect("/")
-    #ver se o nome faz sentido
     def validating_user(self):
         username = request.forms.get("username")
         email = request.forms.get("email")
         password = request.forms.get("password")
-        # dps ver como usar a id correta, esse metodo nao pode ta certo
-        user = User("1", username, email, password)
-        if (user_manager.procura_usuario(user)):
+
+        if (user_manager.procura_usuario(username, password)):
             return True
         else:
             return False
--- data_managers/user_manager.py
+++ data_managers/user_manager.py
@@ -1,6 +1,5 @@
 import json
 
-# talvez fazer um controle de erros depois 
 
 def save_user(user):
     with open("data/users.json", "r") as file:
@@ -10,21 +9,16 @@ def save_user(user):
     with open("data/users.json", "w") as file:
         json.dump(updated_json, file, indent=4)
 
-# essa funcao nao tem nada a ver com user, temos que tirar ela daq
 def create_json_file(filename):
     json_list = []
     with open("data/" + filename,mode="w") as file:
         json.dump(json_list, file, indent=4)
-# tratamento de erros
 def load_users():
     with open("data/users.json", "r") as file:
         users_list = json.load(file)
         return users_list
-# melgorar o for (i)
-# n sei se isso era pra estar aq
-# melhorar nome
-# n eh possivel q tem varias funcoes so retornando true isso nao pode ta certo
-def procura_usuario(user):
+
+def procura_usuario(username, password):
     for i in load_users():
-        if (i["name"] == user.username and i["password"] == user.password):
+        if (i["name"] == username and i["password"] == password):
             return True
